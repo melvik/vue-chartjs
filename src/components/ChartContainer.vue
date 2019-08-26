32af7ae420b934dff664f9f93b0fffac282eed31
Hovik <hovik@Hoviks-MBP.home>
2019-08-25 22:53:28 -0700

    push to repo

---
+++ src/components/ChartContainer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <bar-chart :chartdata="chartdata" :options="options" />
+
+    <radar-chart :chartdata="chartdata" :options="options" />
+  </div>
+</template>
+
+<script>
+import BarChart from "./chart.vue"
+import RadarChart from "./rchart.vue"
+
+export default {
+  name: "LineChartContainer",
+  components: {
+    BarChart,
+    RadarChart
+  },
+  data() {
+    return {
+      chartdata: {
+        labels: ["January", "February", "March", "April", 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: "Months",
+            borderColor: '#f87676',
+            // backgroundColor: ["#f87676", "#abc001", "#b10678", "#676700"],
+            data: [55, 22, 33, 44, 24, 27, 31],
+            minBarLength: 0
+          }
+        ]
+      },
+      options: {
+        responsive: false,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/chart1.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        charts :)
+        <!-- <bar-chart :chartdata="chartData" :options="chartOptions"/> -->
+        <bar-chart/>
+    </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+  extends: Bar,
+  data: () => ({
+    chartdata: {
+      labels: ['January', 'February'],
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#f87979',
+          data: [40, 20]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
+
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
